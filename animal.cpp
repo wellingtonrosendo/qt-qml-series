2e2fd63b62fca07c0de191489e13658e40c031be
wellingtonrosendo <wellington.a.rosendo@gmail.com>
2024-04-22 02:36:43 -0300

    connection with api

---
+++ animal.cpp
@@ -0,0 +1,5 @@
+#include "animal.h"
+
+Animal::Animal(QObject *parent)
+    : QObject{parent}
+{}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "SeriesSearch.h"
+// #include "model.h"
+#include <QtQuick/qquickitem.h>
+#include <QtQuick/qquickview.h>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    // AnimalModel model;
+    // model.addAnimal(Animal("Wolf", "Medium"));
+    // model.addAnimal(Animal("Polar bear", "Large"));
+    // model.addAnimal(Animal("Quoll", "Small"));
+
+    // QQuickView view;
+    // view.setResizeMode(QQuickView::SizeRootObjectToView);
+    // view.setInitialProperties({{"model", QVariant::fromValue(&model)}});
+    // //![0]
+    // view.setSource(QUrl("qrc:/qt-qml-series/view.qml"));
+    // view.show();
+
+    // return app.exec();
+
     QQmlApplicationEngine engine;
+
+    SeriesSearch *seriesSearch = new SeriesSearch(&app);
+    qmlRegisterSingletonInstance("SeriesSearch",1,0,"SeriesSearch", seriesSearch);
+
     const QUrl url(QStringLiteral("qrc:/qt-qml-series/Main.qml"));
     QObject::connect(
         &engine,
@@ -14,6 +36,5 @@ int main(int argc, char *argv[])
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
     engine.load(url);
-
     return app.exec();
 }
